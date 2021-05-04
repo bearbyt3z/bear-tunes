948994118b338bef1c62f3525892bfa147f47c8a
bearbyt3z <bearbyt3z@gmail.com>
2021-05-04 20:26:16 +0200

    Indentation format correction (4 spaces => 2 spaces)

--- logger.js
+++ logger.js
@@ -12,48 +12,48 @@ const level = process.env.LOG_LEVEL || 'debug';
 const colorizer = winston.format.colorize();
 
 // const loggerFormat = winston.format.printf(({ level, message, timestamp }) => {
-//     // return `${new Date(timestamp).toLocaleTimeString()} ${level.toUpperCase()}: ${message}`;
-//     return `${timestamp} ${level.toUpperCase()}: ${message}`;
+//   // return `${new Date(timestamp).toLocaleTimeString()} ${level.toUpperCase()}: ${message}`;
+//   return `${timestamp} ${level.toUpperCase()}: ${message}`;
 // });
 
 const options = {
-    level: level,
-    // format: winston.format.json(),
-    transports: [
-        new winston.transports.Console({
-            level: 'info',
-            format: winston.format.combine(
-                // winston.format.printf(info => `${info.level.toUpperCase()}: ${info.message}`),
-                winston.format.printf(info => info.message),
-                winston.format.colorize({ all: true }),
-            ),
-        }),
-        new winston.transports.File({
-            level: 'error',
-            filename: './logs/errors.log',
-            format: winston.format.combine(
-                // winston.format.label({ label: 'right meow!' }),
-                // winston.format.simple(),
-                // winston.format.prettyPrint(),
-                // // winston.format.json(),
-                // winston.format.align(),
-                winston.format.timestamp({ format: 'YY.MM.DD HH:MM:SS' }),
-                winston.format.printf(info => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`),
-                // winston.format.colorize({ all: true }),
-            )
-        }),
-        new winston.transports.File({ filename: './logs/combined.log' }),
-    ],
-    exceptionHandlers: [
-        new winston.transports.File({ filename: './logs/exceptions.log' })
-    ],
+  level: level,
+  // format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+        // winston.format.printf(info => `${info.level.toUpperCase()}: ${info.message}`),
+        winston.format.printf(info => info.message),
+        winston.format.colorize({ all: true }),
+      ),
+    }),
+    new winston.transports.File({
+      level: 'error',
+      filename: './logs/errors.log',
+      format: winston.format.combine(
+        // winston.format.label({ label: 'right meow!' }),
+        // winston.format.simple(),
+        // winston.format.prettyPrint(),
+        // // winston.format.json(),
+        // winston.format.align(),
+        winston.format.timestamp({ format: 'YY.MM.DD HH:MM:SS' }),
+        winston.format.printf(info => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`),
+        // winston.format.colorize({ all: true }),
+      )
+    }),
+    new winston.transports.File({ filename: './logs/combined.log' }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: './logs/exceptions.log' })
+  ],
 }
 
 // winston.addColors({
-//     error: 'red',
-//     warn: 'yellow',
-//     info: 'cyan',
-//     debug: 'green'
+//   error: 'red',
+//   warn: 'yellow',
+//   info: 'cyan',
+//   debug: 'green'
 // });
 
 module.exports = winston.createLogger(options);
--- tools.js
+++ tools.js
@@ -4,115 +4,115 @@ const fs = require('fs');
 const request = require('request');
 
 module.exports = {
-    fetchWebPage: async url => {
-        const response = await fetch(url)
-            .then(response => response.text())
-            .catch(error => console.error(error));
-        return (new jsdom.JSDOM(response)).window.document;
-    },
+  fetchWebPage: async url => {
+    const response = await fetch(url)
+      .then(response => response.text())
+      .catch(error => console.error(error));
+    return (new jsdom.JSDOM(response)).window.document;
+  },
 
-    arrayDifference: (array1, array2) => array1.filter(value => !array2.includes(value)),
-    arrayIntersection: (array1, array2) => array1.filter(value => array2.includes(value)),
+  arrayDifference: (array1, array2) => array1.filter(value => !array2.includes(value)),
+  arrayIntersection: (array1, array2) => array1.filter(value => array2.includes(value)),
 
-    arrayToLowerCase: array => array.map(value => (value.toLowerCase instanceof Function) ? value.toLowerCase() : value),
+  arrayToLowerCase: array => array.map(value => (value.toLowerCase instanceof Function) ? value.toLowerCase() : value),
 
-    downloadFile: (uri, filename, callback) => new Promise((resolve, reject) => {
-        //request.head(uri, async (error, response, body) => {
-        // console.log('content-type:', response.headers['content-type']);
-        // console.log('content-length:', response.headers['content-length']);
-        const uriSplit = uri.split('/');
-        const uriFilename = uriSplit[uriSplit.length - 1];
-        if (!filename || filename.length < 1) {
-            filename = uriFilename;
-        }
-        else if (filename.split('.').length < 2) {  // no extension
-            const uriFilenameSplit = uriFilename.split('.')
-            const uriFilenameExtension = uriFilenameSplit[uriFilenameSplit.length - 1];
-            filename = filename + uriFilenameExtension;
-        }
-        // request(uri).pipe(fs.createWriteStream(filename)).on('close', callback(filename));
-        request(uri).pipe(fs.createWriteStream(filename))
-            .on('close', () => {
-                resolve(`File created successfully: ${filename}`);
-                callback(filename);
-            })
-            .on('error', error => reject(error));
-        // request(uri).pipe(fs.WriteSync(filename)).on('close', callback(filename));
-    }),
+  downloadFile: (uri, filename, callback) => new Promise((resolve, reject) => {
+    //request.head(uri, async (error, response, body) => {
+    // console.log('content-type:', response.headers['content-type']);
+    // console.log('content-length:', response.headers['content-length']);
+    const uriSplit = uri.split('/');
+    const uriFilename = uriSplit[uriSplit.length - 1];
+    if (!filename || filename.length < 1) {
+      filename = uriFilename;
+    }
+    else if (filename.split('.').length < 2) {  // no extension
+      const uriFilenameSplit = uriFilename.split('.')
+      const uriFilenameExtension = uriFilenameSplit[uriFilenameSplit.length - 1];
+      filename = filename + uriFilenameExtension;
+    }
+    // request(uri).pipe(fs.createWriteStream(filename)).on('close', callback(filename));
+    request(uri).pipe(fs.createWriteStream(filename))
+      .on('close', () => {
+        resolve(`File created successfully: ${filename}`);
+        callback(filename);
+      })
+      .on('error', error => reject(error));
+    // request(uri).pipe(fs.WriteSync(filename)).on('close', callback(filename));
+  }),
 
-    // replaceFilenameExtension: filename => filename.replace(/\.[^\\/.]+$/, ''),  // it's easier to use path module
+  // replaceFilenameExtension: filename => filename.replace(/\.[^\\/.]+$/, ''),  // it's easier to use path module
 
-    splitTrackNameToKeywords: name => {
-        if (name instanceof Array) name = name.join(' ')
-        name = name.trim();  // remove spaces at the beggining & end
-        name = name.replace(/\s+[-–&]\s+|\s+/mgi, ' ');
-        // name = name.replace(/[\(\)\[\],]|\.[\w\d]+?$/mgi, '');  // +? => non-greedy for file extension match  // don't work with: Lust 2.1.mp3
-        name = name.replace(/[\(\)\[\],]|\.mp3$/mgi, '');  // +? => non-greedy for file extension match
-        // console.log(name);
-        return Array.from(new Set(name.split(' ')));  // set to avoid repetitions
-        // return name.match(/\b([\w\d]+)\b/mgi);
-    },
+  splitTrackNameToKeywords: name => {
+    if (name instanceof Array) name = name.join(' ')
+    name = name.trim();  // remove spaces at the beggining & end
+    name = name.replace(/\s+[-–&]\s+|\s+/mgi, ' ');
+    // name = name.replace(/[\(\)\[\],]|\.[\w\d]+?$/mgi, '');  // +? => non-greedy for file extension match  // don't work with: Lust 2.1.mp3
+    name = name.replace(/[\(\)\[\],]|\.mp3$/mgi, '');  // +? => non-greedy for file extension match
+    // console.log(name);
+    return Array.from(new Set(name.split(' ')));  // set to avoid repetitions
+    // return name.match(/\b([\w\d]+)\b/mgi);
+  },
 
-    createTitle: (titleNode, remixedNode) => {
-        let title = titleNode.textContent.trim();
-        if (title.match(/\bfeat\b/i)) {
-            title = title.replace(/\bfeat\.? /i, 'feat. ');  // add missing dot after "feat" shortcut, and replace "Feat" with "feat"
-            if (title.indexOf('(feat') < 0) {  // if "feat" isn't in parentheses add them
-                title = title.replace(/\bfeat. /, '(feat. ') + ')';
-            }
-        }
-        if (remixedNode) title = `${title} (${remixedNode.textContent.trim()})`;
-        return title;
-    },
+  createTitle: (titleNode, remixedNode) => {
+    let title = titleNode.textContent.trim();
+    if (title.match(/\bfeat\b/i)) {
+      title = title.replace(/\bfeat\.? /i, 'feat. ');  // add missing dot after "feat" shortcut, and replace "Feat" with "feat"
+      if (title.indexOf('(feat') < 0) {  // if "feat" isn't in parentheses add them
+        title = title.replace(/\bfeat. /, '(feat. ') + ')';
+      }
+    }
+    if (remixedNode) title = `${title} (${remixedNode.textContent.trim()})`;
+    return title;
+  },
 
-    createArtistsList: (artistsNode, title = '') => {  // if title provided => remove featuring artists from artist list
-        if (!artistsNode) return '';  // '' => delete frame if there is no artist information
-        const artistsLinks = artistsNode.querySelectorAll('a');
-        if (artistsLinks.length > 0) {
-            return Array.from(artistsLinks).reduce((result, link) => {
-                const artist = link.textContent.trim();
-                if (title.search(new RegExp(`(feat|ft).+${module.exports.regExpEscape(artist)}`, 'i')) < 0)  // we have to search for feat/ft before artist name
-                    result.push(artist);
-                return result;
-            }, []).join(', ');
-        }
-        return artistsNode.textContent.trim();
-        // return (artistsLinks.length > 0) ? Array.from(artistsLinks).map(link => link.textContent.trim())).join(', ') : artistsNode.textContent.trim();
-    },
+  createArtistsList: (artistsNode, title = '') => {  // if title provided => remove featuring artists from artist list
+    if (!artistsNode) return '';  // '' => delete frame if there is no artist information
+    const artistsLinks = artistsNode.querySelectorAll('a');
+    if (artistsLinks.length > 0) {
+      return Array.from(artistsLinks).reduce((result, link) => {
+        const artist = link.textContent.trim();
+        if (title.search(new RegExp(`(feat|ft).+${module.exports.regExpEscape(artist)}`, 'i')) < 0)  // we have to search for feat/ft before artist name
+          result.push(artist);
+        return result;
+      }, []).join(', ');
+    }
+    return artistsNode.textContent.trim();
+    // return (artistsLinks.length > 0) ? Array.from(artistsLinks).map(link => link.textContent.trim())).join(', ') : artistsNode.textContent.trim();
+  },
 
-    createGenresList: (genresNode) => {
-        if (!genresNode) return '';  // '' => delete frame if there is no genre information
-        const genresLinks = genresNode.querySelectorAll('a');
-        if (genresLinks.length > 0) {
-            return Array.from(genresLinks).reduce((result, link) => {
-                const genre = link.textContent.trim();
-                const separator = result && (link.href.indexOf('sub-genre') >= 0 ? ': ' : ', ');  // only if result != ''
-                return result + separator + genre;
-            }, '');
-        }
-        return genresNode.textContent.trim();
-        // return (artistsLinks.length > 0) ? Array.from(artistsLinks).map(link => link.textContent.trim())).join(', ') : artistsNode.textContent.trim();
-    },
+  createGenresList: (genresNode) => {
+    if (!genresNode) return '';  // '' => delete frame if there is no genre information
+    const genresLinks = genresNode.querySelectorAll('a');
+    if (genresLinks.length > 0) {
+      return Array.from(genresLinks).reduce((result, link) => {
+        const genre = link.textContent.trim();
+        const separator = result && (link.href.indexOf('sub-genre') >= 0 ? ': ' : ', ');  // only if result != ''
+        return result + separator + genre;
+      }, '');
+    }
+    return genresNode.textContent.trim();
+    // return (artistsLinks.length > 0) ? Array.from(artistsLinks).map(link => link.textContent.trim())).join(', ') : artistsNode.textContent.trim();
+  },
 
-    createKey: keyNode => keyNode.textContent.trim().replace('♭ ', 'b').replace('♯ ', '#').replace('maj', 'M').replace('min', 'm'),
+  createKey: keyNode => keyNode.textContent.trim().replace('♭ ', 'b').replace('♯ ', '#').replace('maj', 'M').replace('min', 'm'),
 
-    isString: value => typeof value === 'string' || value instanceof String,
+  isString: value => typeof value === 'string' || value instanceof String,
 
-    regExpEscape: (str) => String(str).replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'),
+  regExpEscape: (str) => String(str).replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'),
 
-    replacePathForbiddenChars: stringOrArray => {
-        const replaceRegEx = /[\/\\\*\?\<\>|:"]/gm;
-        if (module.exports.isString(stringOrArray)) return stringOrArray.replace(replaceRegEx, '-');
-        if (stringOrArray.map instanceof Function) return stringOrArray.map(str => str.replace(replaceRegEx, '-'));
-        return stringOrArray;  // in other case...
-    },
+  replacePathForbiddenChars: stringOrArray => {
+    const replaceRegEx = /[\/\\\*\?\<\>|:"]/gm;
+    if (module.exports.isString(stringOrArray)) return stringOrArray.replace(replaceRegEx, '-');
+    if (stringOrArray.map instanceof Function) return stringOrArray.map(str => str.replace(replaceRegEx, '-'));
+    return stringOrArray;  // in other case...
+  },
 
-    leaveOnlyFirstLine: text => text.replace(/\n.*/gmi, ''),
+  leaveOnlyFirstLine: text => text.replace(/\n.*/gmi, ''),
 
-    getUrlFromFile: filePath => {
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        const urlIndex = fileContent.indexOf('URL=');
-        if (!urlIndex) return null;
-        return fileContent.substring(urlIndex + 4).split('\n')[0];
-    },
+  getUrlFromFile: filePath => {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const urlIndex = fileContent.indexOf('URL=');
+    if (!urlIndex) return null;
+    return fileContent.substring(urlIndex + 4).split('\n')[0];
+  },
 };
