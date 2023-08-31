2dee77a18d6c5e78e429a2fcbc9af56db1600140
bearbyt3z <bearbyt3z@gmail.com>
2023-08-31 04:08:03 +0200

    If there is no tag info, eyed3-display-plugin script will return object with empty tag entries rather than throwing an error

--- eyed3-display-plugin.py
+++ eyed3-display-plugin.py
@@ -17,30 +17,27 @@ if len(pattern) < 6:
 eyed3.log.setLevel('ERROR') # prevent from printing warnings e.g.: Non standard genre name: ...
 
 audio = eyed3.load(sys.argv[2])
-if audio.tag is None:
-  print(f'Error: ID3 tag not found in "{sys.argv[2]}"', file=sys.stderr)
-  sys.exit(3)
 
-pattern = pattern.replace('%artist%', str(audio.tag.artist or ''))
-pattern = pattern.replace('%title%', str(audio.tag.title or ''))
-pattern = pattern.replace('%release-date%', str(audio.tag.release_date or ''))
-pattern = pattern.replace('%genre%', str(audio.tag.genre or ''))
-pattern = pattern.replace('%audio-file-url%', str(audio.tag.audio_file_url or ''))
-pattern = pattern.replace('%comments%', str(audio.tag.comments or ''))
-pattern = pattern.replace('%music-cd-id%', str(audio.tag.cd_id or ''))
-pattern = pattern.replace('%publisher%', str(audio.tag.publisher or ''))
-pattern = pattern.replace('%publisher-url%', str(audio.tag.publisher_url or ''))
+pattern = pattern.replace('%artist%', str(audio.tag and audio.tag.artist or ''))
+pattern = pattern.replace('%title%', str(audio.tag and audio.tag.title or ''))
+pattern = pattern.replace('%release-date%', str(audio.tag and audio.tag.release_date or ''))
+pattern = pattern.replace('%genre%', str(audio.tag and audio.tag.genre or ''))
+pattern = pattern.replace('%audio-file-url%', str(audio.tag and audio.tag.audio_file_url or ''))
+pattern = pattern.replace('%comments%', str(audio.tag and audio.tag.comments or ''))
+pattern = pattern.replace('%music-cd-id%', str(audio.tag and audio.tag.cd_id or ''))
+pattern = pattern.replace('%publisher%', str(audio.tag and audio.tag.publisher or ''))
+pattern = pattern.replace('%publisher-url%', str(audio.tag and audio.tag.publisher_url or ''))
 
 # Album entries:
-pattern = pattern.replace('%album%', str(audio.tag.album or ''))
-pattern = pattern.replace('%album-artist%', str(audio.tag.album_artist or ''))
-pattern = pattern.replace('%track%', str(audio.tag.track_num.count or ''))
-pattern = pattern.replace('%track-total%', str(audio.tag.track_num.total or ''))
+pattern = pattern.replace('%album%', str(audio.tag and audio.tag.album or ''))
+pattern = pattern.replace('%album-artist%', str(audio.tag and audio.tag.album_artist or ''))
+pattern = pattern.replace('%track%', str(audio.tag and audio.tag.track_num.count or ''))
+pattern = pattern.replace('%track-total%', str(audio.tag and audio.tag.track_num.total or ''))
 
-pattern = pattern.replace('$length()', str(audio.info.time_secs or ''))
+pattern = pattern.replace('$length()', str(audio.info and audio.info.time_secs or ''))
 
 user_text_frames = ''
-for frame in audio.tag.user_text_frames:
+for frame in audio.tag and audio.tag.user_text_frames or []:
   user_text_frames += f'"{frame.description}": "{frame.text}"\, '
 
 pattern = pattern.replace('%texts,output="#d": "#t"\,%', user_text_frames)
